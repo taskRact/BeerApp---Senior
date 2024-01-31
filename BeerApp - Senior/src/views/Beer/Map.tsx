64fd539b86ac27e75dad39138eda19a819082d30
JABER-AHMED <placidcse@gmail.com>
2024-01-31 11:46:51 +0100

    fix map market issue

--- BeerApp - Senior/src/views/Beer/Map.tsx
+++ BeerApp - Senior/src/views/Beer/Map.tsx
@@ -1,6 +1,13 @@
+import "leaflet/dist/leaflet.css";
 import React, { useRef } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+
+import L from "leaflet";
+
+const customIcon = L.icon({
+    iconUrl: `${window.location.origin}/images/marker-icon.png`,
+    iconSize: [25, 30],
+});
 
 interface MapProps {
     lat: number;
@@ -15,12 +22,12 @@ const Map: React.FC<MapProps> = ({ lat, lang, name }) => {
     const companyName = name;
 
     return (
-        <MapContainer center={[latitude, longitude]} zoom={15} ref={mapRef} style={{height: "70vh", width: "40vw"}}>
+        <MapContainer center={[latitude, longitude]} zoom={15} ref={mapRef} style={{height: "70vh", width: "70vw"}}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={[latitude, longitude]}>
+            <Marker position={[latitude, longitude]} icon={customIcon}>
                 <Popup>
                     {companyName}
                 </Popup>
